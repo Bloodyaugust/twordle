07361abdb4f645d701665e845fa70d5b3dbd488c
Bloodyaugust <greysonrichey@gmail.com>
2023-03-18 22:45:15 -0600

    Add a whole bunch of project config stuff

---
+++ src/welcome/Welcome.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Welcome() {
+  return (
+    <div className="col-span-4 self-center justify-self-center border-solid border-2 rounded-sm p-4">
+      <h2 className="text-center">Welcome to Twordle!</h2>
+    </div>
+  );
+}
---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+// eslint-disable-next-line
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};

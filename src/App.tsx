e36cd9fef61e56820bcb71f18f841ae6e7ae5f70
Greyson Richey <greysonrichey@gmail.com>
2023-03-30 00:44:10 -0600

    Add icon library
    - Fix more usability and layout issues for mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
+import { FaGamepad, FaHome, FaUser } from 'react-icons/fa';
 import './App.css';
 import { User } from './user/User';
 
 function App() {
   return (
-    <div className="App grid grid-cols-4 gap-2 bg-slate-300 p-6 dark:bg-slate-800 dark:text-white">
+    <div className="App grid gap-2 bg-slate-300 p-6 dark:bg-slate-800 dark:text-white md:grid-cols-4">
       <User>
         <div className="header text-center text-xl font-bold">
           <h1>Twordle</h1>
         </div>
         <nav className="nav flex flex-col gap-4">
-          <NavLink className={({ isActive }) => `underline ${isActive && 'text-violet-600'}`} to="" end>
-            Home
+          <NavLink
+            className={({ isActive }) => `flex items-center gap-2 underline ${isActive && 'text-violet-600'}`}
+            to=""
+            end>
+            <FaHome />
+            <span className="hidden md:inline">Home</span>
           </NavLink>
-          <NavLink className={({ isActive }) => `underline ${isActive && 'text-violet-600'}`} to="games">
-            Games
+          <NavLink
+            className={({ isActive }) => `flex items-center gap-2 underline ${isActive && 'text-violet-600'}`}
+            to="games">
+            <FaGamepad />
+            <span className="hidden md:inline">Games</span>
           </NavLink>
-          <NavLink className={({ isActive }) => `underline ${isActive && 'text-violet-600'}`} to="profile">
-            Profile
+          <NavLink
+            className={({ isActive }) => `flex items-center gap-2 underline ${isActive && 'text-violet-600'}`}
+            to="profile">
+            <FaUser />
+            <span className="hidden md:inline">Profile</span>
           </NavLink>
         </nav>
         <div className="content overflow-x-scroll">
--- src/Game/Game.tsx
+++ src/Game/Game.tsx
@@ -145,7 +145,7 @@ export default function Game() {
 
   if (!gameOver && userPicking) {
     return (
-      <div className="flex gap-2">
+      <div className="flex flex-col gap-2 md:flex-row">
         <span>You are picking a word for your opponent:</span>
         <Input
           id="opponent-word-pick"
